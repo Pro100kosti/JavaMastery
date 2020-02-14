4de36f43df7c791a3cac7648727ab4e823a5f0ff
Pro100kosti <pro100kosti@gmail.com>
2020-02-14 13:24:45 +0300

    Day 1 :)

---
+++ src/main/java/by/bolotko/GreetingController.java
@@ -0,0 +1,17 @@
+package by.bolotko;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @RequestMapping(value = "/greeting", method = RequestMethod.GET)
+    public String greeting() {
+        return "greeting";
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/style/home.css">
+    <title>Spring Boot</title>
+</head>
+<body>
+    <h2>Hello spring boot JSP!!</h2>
+</body>
+</html>
